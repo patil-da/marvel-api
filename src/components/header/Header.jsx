7a874d9d1e8c8d5db53754ab92b2c9cf1a01966b
patil-da <95049289+patil-da@users.noreply.github.com>
2022-08-03 00:17:57 +0100

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import logo from '../../images/marvel-logo.png'
 
+const Header = () => {
+  return (
+    <header className='header'>
+        <img src={logo} alt=''/>
+    </header>
+  )
+}
+
+export default Header;
